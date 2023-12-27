5f085e8b5f9f131c6ff1e7c921a0d3526f794a78
sanikakachare1010 <130974722+sanikakachare1010@users.noreply.github.com>
2023-12-27 05:45:38 -0800

    Update Hello.java

--- Basic/Hello.java
+++ Basic/Hello.java
@@ -2,9 +2,9 @@ public class Number
 {
   public static void main(String args[])
   {
-     System.out.println("Hello World");//
+     System.out.println("Hello World");
 
-     System.out.println("24"+26);//2426
+     System.out.println("24"+26);
      System.out.println(24+26);
 
   }
