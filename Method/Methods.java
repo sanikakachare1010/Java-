f4c14fd58f623ac0a253a74e1674514bfaed3b88
sanikakachare1010 <130974722+sanikakachare1010@users.noreply.github.com>
2024-01-01 08:50:49 -0800

    Method

---
+++ Method/Methods.java
@@ -0,0 +1,28 @@
+package Com.Sgm.Methods;
+
+public class Methods
+{
+	public void Show()
+	{
+		for(int i = 0;i<5;i++)
+		{
+			System.out.println("India");
+		}
+		int i=1;
+		while(i<=5)
+		{
+			System.out.println("India");
+			i++;
+		}
+	}
+	public static void main(String[] args)
+	{
+		
+		Methods A = new Methods();
+	    A.Show();
+		//A.Show();
+		
+		
+	}
+
+}
