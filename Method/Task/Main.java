15453316aeabaa9098e444f0e143897f8bc54552
sanikakachare1010 <130974722+sanikakachare1010@users.noreply.github.com>
2024-01-02 07:20:14 -0800

    Method

---
+++ Method/Task/Main.java
@@ -0,0 +1,19 @@
+package Method.Task2;
+
+public class Main 
+{
+	public static void main(String[] args)
+	{
+		Task04 T = new Task04();
+		T.Armstrong();
+		
+		Palindrome P = new Palindrome();
+		P.palin();
+		
+		PrimeNum p = new PrimeNum();
+		p.prime();
+		
+		
+	}
+
+}
