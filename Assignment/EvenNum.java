012265cef4000685455da0c4afa9d1d0a0aed466
sanikakachare1010 <130974722+sanikakachare1010@users.noreply.github.com>
2024-01-12 07:52:24 -0800

    Practice

---
+++ Assignment/EvenNum.java
@@ -0,0 +1,17 @@
+package Com.Sgm.Assignment;
+
+public class EvenNum 
+{
+	public static void main(String[] args)
+	{
+		for(int i =1;i<=10;i++)
+		{
+			if(i%2==0)
+			{
+				System.out.println(i);
+			}
+		}
+		
+	}
+
+}
