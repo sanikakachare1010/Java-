94caa9392b146f7d3f5553f2d8c33eb96716412a
sanikakachare1010 <130974722+sanikakachare1010@users.noreply.github.com>
2024-01-16 09:38:40 -0800

    Polymorphism

---
+++ Polymorphism/Overloading/A.java
@@ -0,0 +1,18 @@
+package Com.Sgm.Overriding;
+
+public class A
+{
+	public void M1()
+	{
+		System.out.println("M1 Method From A");
+	}
+	public void M2()
+	{
+		System.out.println("M2 Method From A");
+	}
+	public void M5()
+	{
+		System.out.println("M5 Method  From A");
+	}
+
+}
---
+++ Polymorphism/Overloading/B.java
@@ -0,0 +1,18 @@
+package Com.Sgm.Overriding;
+
+public class B extends A
+{
+	public void M1()
+	{
+		System.out.println("M1 method From B");
+	}
+	public void M2()
+	{
+		System.out.println("M2 Method From B");
+	}
+	public void M3()
+	{
+		System.out.println("M3 Method From B");
+	}
+
+}
